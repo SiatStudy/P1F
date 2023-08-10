88537fc81a0623e7bba8efa761666455db7ecd79
ChangYoon <pcyoon9989@gmail.com>
2023-08-10 09:03:54 +0900

    리덕스 세팅중
    자꾸 에러남. 문제 해결 필요

--- src/store/todoData.js
+++ src/store/todoData.js
@@ -2,27 +2,43 @@ import { createSlice } from "@reduxjs/toolkit";
 
 let todoData = createSlice({
     name : "todoData",
-    initialState : [],
+    initialState : [{
+        tdid : "",
+        month: 0,
+        startDay: 0,
+        endDay: 0,
+        workTitle: ""
+}],
     reducers : {
         setTodoData : (state, action) => {
-            state = action.payload;
-        }, //백엔드에서 todo데이터 객체 배열 생성 함수
+            //백엔드에서 todo데이터 객체 배열 생성 함수
+            return action.payload;
+        }, 
         addTodoData : (state, action) => {
+            //할일 추가 시 객체를 받아 배열에 추가하는 함수
             state.push(action.payload);
-        }, //할일 추가 시 객체를 받아 배열에 추가하는 함수
+        }, 
         modifyTodoData : (state, action) => {
-            state.map((item) =>{
-              if(item==item)
-            } // 어떤 기준으로 수정하는지
-        }, // 할일 수정 시 수정할 tdid, 변경할 키값, 변경할 value 받아서 수정
+            // 어떤 기준으로 수정하는지
+            // 할일 수정 시 수정할 tdid, 변경할 키값, 변경할 value 받아서 수정
+            return state.map((item) =>{
+              if(item.tdid==action.payload.tdid){
+                item = {
+                    ...item,
+                    [action.payload.key] : action.payload.value
+                }
+              } else {
+                item;
+              }
+            } );
+        }, 
         delTodoData : (state, action) => {
-            state.map((item) =>{
-              if(item==item)
-              state.filter("로직");
-            } //어떤 기준으로 삭제하는지
-        } // 할일 삭제 시 삭제할 tdid 받아서 삭제
+            // 어떤 기준으로 삭제하는지
+            // 할일 삭제 시 삭제할 tdid 받아서 삭제
+            return state.filter((item)=>{item.tdid!=action.payload});            
+        } 
     } // 이렇게 추가, 수정, 삭제하는 axios api로직도 만들어야됨
 });
 
-export const { setTodoData } = todoData.actions;
+export const { setTodoData, addTodoData, modifyTodoData, delTodoData } = todoData.actions;
 export default todoData.reducer;
